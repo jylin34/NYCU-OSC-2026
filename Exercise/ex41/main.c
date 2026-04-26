a9fc516badbe11a0aa9bf78c6f613e4cdaf8d606
jylin <jinyulin34@gmail.com>
2026-04-26 08:20:18 +0800

    [ex41] finish

--- Exercise/ex41/main.c
+++ Exercise/ex41/main.c
@@ -8,7 +8,7 @@ extern int memcmp(const void* s1, const void* s2, int n);
 extern void* alloc_page();
 
 // TODO: Check the RAM disk base address
-#define INITRD_BASE 0xa0200000
+#define INITRD_BASE 0x0000000088200000
 #define STACK_SIZE  0x1000
 
 struct cpio_t {
@@ -28,6 +28,13 @@ struct cpio_t {
     char check[8];
 };
 
+// 定義寄存器結構，對應 start.S 的儲存順序
+struct pt_regs {
+    unsigned long ra, sp, gp, tp, t0, t1, t2, s0, s1, a0, a1, a2, a3, a4, a5, a6, a7, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, t3, t4, t5, t6;
+    unsigned long sepc, sstatus, scause, stval;
+};
+
+// 在 Initramfs 尋找 prog.bin, 並且在 U-Mode 執行它
 int exec(const char* filename) {
     char* p = (char*)INITRD_BASE;
     while (memcmp(p + sizeof(struct cpio_t), "TRAILER!!!", 10)) {
@@ -36,21 +43,47 @@ int exec(const char* filename) {
         int filesize = hextoi(hdr->filesize, 8);
         int headsize = align(sizeof(struct cpio_t) + namesize, 4);
         int datasize = align(filesize, 4);
+        
         if (!memcmp(p + sizeof(struct cpio_t), filename, namesize)) {
-            // TODO: Finish this function
+            unsigned long target_address = (unsigned long)(p + headsize);
+            void* user_stack = alloc_page();
+            if (!user_stack) return -1;
+
+            unsigned long user_sp = (unsigned long)user_stack + STACK_SIZE;
+            unsigned long sstatus;
+
+            // 設定 sstatus: SPP=0 (返回 U-mode), SPIE=1 (開啟中斷)
+            asm volatile("csrr %0, sstatus" : "=r"(sstatus));
+            sstatus &= ~(1L << 8); // 清除 SPP 位元 (Bit 8)
+            sstatus |= (1L << 5);  // 設定 SPIE 位元 (Bit 5)
+
+            // 準備跳轉至 User mode
+            // sscratch 必須存入當前核心堆疊，以便下次進入 Trap 時切換回來
+            asm volatile(
+                "csrw sepc, %0\n"
+                "csrw sstatus, %1\n"
+                "csrw sscratch, sp\n" 
+                "mv sp, %2\n"
+                "sret\n"
+                : : "r"(target_address), "r"(sstatus), "r"(user_sp)
+            );
         }
         p += headsize + datasize;
     }
     return -1;
 }
 
-// TODO: Define the trap frame structure
-struct pt_regs {};
-
+// 被 start.S 呼叫
 void do_trap(struct pt_regs* regs) {
-    // TODO: Implement this function
-    // (1) Print the sepc and scause registers
-    // (2) Increment the sepc register by 4 for traps
+    // (1) 打印 sepc 與 scause
+    uart_puts("Trap! sepc: ");
+    uart_hex(regs->sepc);
+    uart_puts(", scause: ");
+    uart_hex(regs->scause);
+    uart_puts("\n");
+
+    // (2) ecall 指令長度為 4，將 sepc 加 4 以跳過該指令
+    regs->sepc += 4;
 }
 
 void start_kernel() {
